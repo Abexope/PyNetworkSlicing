e50276cdc69c87926ec0d340321dd97d2cd58736
Xing Xudong <50110585+abexope@users.noreply.github.com>
2019-12-02 09:36:21 +0800

    添加base包，将队列结构移至其中

--- evironment/scenario.py
+++ evironment/scenario.py
@@ -5,84 +5,11 @@
 
 from random import randint, seed
 from math import ceil
-
+from base.queue import Queue, PriorQueue
 
 # 全局变量
 DURATION = 2000     # 一帧1000毫秒，2000个slot，每个slot0.5毫秒
 seed(0)
-
-
-class PriorQueue:
-	"""优先队列"""
-	
-	def __init__(self):
-		self._elements = list()        # 基于线性表实现
-		self._length = 0
-	
-	def __len__(self):
-		return self._length
-		
-	def __str__(self):
-		return "[" + " -> ".join([str(item) for item in self._elements]) + "]"
-	
-	def is_empty(self):
-		return not self._elements
-	
-	def enqueue(self, e):
-		"""加入队列"""
-		i = len(self._elements) - 1
-		while i >= 0:
-			if self._elements[i] <= e:
-				i -= 1
-			else:
-				break
-		self._elements.insert(i + 1, e)
-		self._length += 1
-	
-	def peek(self):
-		"""获取优先级最高的元素"""
-		if self.is_empty():
-			raise ValueError("Empty queue!")
-		return self._elements[-1]
-	
-	def dequeue(self):
-		"""出队列"""
-		if self.is_empty():
-			raise ValueError("Empty queue!")
-		self._length -= 1
-		return self._elements.pop()
-
-
-class Queue:
-	"""基本队列"""
-	
-	def __init__(self):
-		self._elements = list()
-		self._length = 0
-	
-	def __len__(self):
-		return self._length
-	
-	def __str__(self):
-		return "[" + " -> ".join([str(item) for item in self._elements[::-1]]) + "]"
-	
-	def is_empty(self):
-		return not self._elements
-	
-	def enqueue(self, e):
-		self._elements.append(e)
-		self._length += 1
-	
-	def peek(self):
-		if self.is_empty():
-			raise ValueError("Empty queue!")
-		return self._elements[0]
-	
-	def dequeue(self):
-		if self.is_empty():
-			raise ValueError("Empty queue!")
-		self._length -= 1
-		return self._elements.pop(0)
 
 
 class Package:
